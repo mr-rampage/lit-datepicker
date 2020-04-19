d248ce2d538a0ad554d1ac1eb2bc255757a949c4
mr-rampage <readonly@che.wbac.ca>
2020-04-19 17:30:58 -0400

    :art: moved utility methods

--- src/components/utils.ts
+++ src/components/utils.ts
@@ -1,3 +1,5 @@
+import { render, TemplateResult } from "lit-html";
+
 export const getLanguage = (): string => window.navigator.languages ? window.navigator.languages[0] : window.navigator['userLanguage'] || window.navigator.language;
 
 export function isSame(previous: Date, next: Date) {
@@ -7,6 +9,34 @@ export function isSame(previous: Date, next: Date) {
     previous.getUTCDate() === next.getUTCDate();
 }
 
-export function compare(a: Date, b: Date) {
-  
+export function replaceInputElement(source: HTMLInputElement) {
+  const replacement = document.createElement('div');
+  source.setAttribute('hidden', '');
+  source.parentElement.insertBefore(replacement, source);
+  return replacement;
+}
+
+export function synchronizeInput(source: HTMLInputElement, ...events: string[]) {
+  return (value: string) => {
+    if (source.value !== value) {
+      source.value = value;
+      events.forEach(event => source.dispatchEvent(new Event(event)));
+    }
+  }
+}
+
+export function pipe(...fns: Function[]) {
+ return (x: any) => fns.reduce((v, f) => f(v), x); 
+}
+
+export function renderOnNext<S>(template: (state: S) => TemplateResult, target: HTMLElement, onNext: (newState: S, oldState: S) => void):
+  (state: S) => Generator<S, void, Partial<S>> {
+  function* store(oldState: S) {
+    render(template(oldState), target);
+    const nextState: S = {...oldState, ...(yield oldState)};
+    onNext(nextState, oldState);
+    yield* store(nextState);
+  }
+
+  return (state: S) => store(state);
 }
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
-import { render, TemplateResult } from 'lit-html';
 import { calendar } from './components/uikit-calendar';
-import { getLanguage } from './components/utils';
+import { getLanguage, replaceInputElement, synchronizeInput, renderOnNext, pipe } from './components/utils';
 import { CalendarProps } from './components';
 
 main();
+
+function main() {
+  document.querySelectorAll('[type=date]').forEach(datePicker);
+}
 
 function datePicker(input: HTMLInputElement) {
   const target = replaceInputElement(input);
@@ -20,7 +23,7 @@ function datePicker(input: HTMLInputElement) {
 
   const sync = (newState: CalendarProps) => synchronize(newState.value)
 
-  const draw = makeRenderLoop(template, target, sync)({
+  const draw = renderOnNext(template, target, sync)({
     value: input.valueAsDate || new Date(),
     month: input.valueAsDate || new Date()
   });
@@ -28,45 +31,8 @@ function datePicker(input: HTMLInputElement) {
   draw.next();
 }
 
-function main() {
-  document.querySelectorAll('[type=date]').forEach(datePicker);
-}
-
-// Utility stuff...
-function replaceInputElement(source: HTMLInputElement) {
-  const replacement = document.createElement('div');
-  source.setAttribute('hidden', '');
-  source.parentElement.insertBefore(replacement, source);
-  return replacement;
-}
-
-function synchronizeInput(source: HTMLInputElement, ...events: string[]) {
-  return (value: string) => {
-    if (source.value !== value) {
-      source.value = value;
-      events.forEach(event => source.dispatchEvent(new Event(event)));
-    }
-  }
-}
-
 function asInputValue(date: Date) {
   const options = { year: 'numeric', month: 'numeric', day: 'numeric' };
   const dateFormatter = new Intl.DateTimeFormat(getLanguage(), options);
   return dateFormatter.format(date);
-}
-
-function pipe(...fns: Function[]) {
- return (x: any) => fns.reduce((v, f) => f(v), x); 
-}
-
-function makeRenderLoop<S>(template: (state: S) => TemplateResult, target: HTMLElement, onNext: (newState: S, oldState: S) => void):
-  (state: S) => Generator<CalendarProps, void, Partial<CalendarProps>> {
-  function* store(oldState: S) {
-    render(template(oldState), target);
-    const nextState: S = {...oldState, ...(yield oldState)};
-    onNext(nextState, oldState);
-    yield* store(nextState);
-  }
-
-  return (state: S) => store(state);
 }
